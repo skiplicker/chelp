2110e1fd6e9f2c67bd01efa948087cbad8bbf44e
skiplicker <skiplicker@dev.null>
2023-03-10 01:39:54 +0000

    updates

--- chelp.py
+++ chelp.py
@@ -12,7 +12,7 @@ os_version = platform.release()
 username = getpass.getuser()
 parser = argparse.ArgumentParser(description='Console helper bot')
 parser.add_argument('query', type=str, help='What do you need help with?')
-parser.add_argument('--user', type=str, default=username, help='Assume a persona to whom the bot wants to speak and offer help!')
+parser.add_argument('--user', type=str, default=username, help='Who is asking?')
 args = parser.parse_args()
 cyan = "\033[36m"
 endpoint = "https://api.openai.com/v1/chat/completions"
@@ -30,24 +30,25 @@ data = {
         { 
             "role": "system",
             "content": """You are a bot that helps developers and system administrators. You are accurate and efficient. This means you provide commands requested!\n
-            You reply in the format: [succinct answer] [one sentence informational text if necessary]
+            You reply in the format: [code or command] [one sentence informational text if necessary]\n
             Current environment: {0} {1}""".format(os_name, os_version)
         },
         {
             "role": "user",
-            "content": "My name is " + args.user +", the user. First question: " + args.query + "? Provide detailed commmands if needed, but otherwise reply as briefly as possible!"
+            "content": "My name is " + args.user +". First question: " + args.query + "? Provide detailed commmands if needed, but otherwise reply as briefly as possible!"
         }
     ]
 }
 try:
     response = requests.post(url=endpoint, headers=headers, json=data)
+    tokens_used = response.json()['usage']['total_tokens']
     message_content = response.json()['choices'][0]['message']['content']
-    print(f"🤖 {cyan}", end='', flush=True)
+    print(f"{tokens_used} 🤖 {cyan}", end='', flush=True)
     for char in message_content:
         print(char, end='', flush=True)
         time.sleep(0.005) 
     print()
     sys.exit(0)
 except (KeyError, ValueError) as e:
-    print(f"Invalid API response: {e}")
+    print(f"Error: {e}")
     sys.exit(2)
